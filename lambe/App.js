7a8fd9e64f48c5ff3c64e6ec042b8fe42032897d
Faitly <fty.57@alu.ufc.br>
2022-01-03 08:37:54 -0300

    Update App.js

--- lambe/App.js
+++ lambe/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React from 'react'
+import { Provider } from 'react-redux' // Envolver toda a aplicação dentro desse componente Provider
 import { View } from 'react-native'
-import Navigation from './src/Navigator'
+
+import Navigator from './src/Navigator'
+import storeConfig from './src/store/storeConfig'
+
+const store = storeConfig()
+const Redux = () => {
+  <Provider store={store}>
+    <View style={{ flex: 1 }}>
+      <Navigator />
+    </View>
+  </Provider>
+}
+// O ponto raiz agora é o componente Provider
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1 }}>
-        {/* No App.js retorna-se apenas as rotas */}
-        <Navigator />
-      </View>
+      <Redux/>
     )
   }
 }
