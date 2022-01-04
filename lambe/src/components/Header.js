66ff591be451bfc2bd71c87a949cfb81fddb27af
Francisco Damasceno <fty.57@alu.ufc.br>
2022-01-04 08:45:09 -0300

    Update Header.js

--- lambe/src/components/Header.js
+++ lambe/src/components/Header.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Gravatar } from 'react-native-gravatar'
 import {
      StyleSheet,
      Text,
@@ -11,12 +13,20 @@ import icon from '../../assets/img/camera.png'
 
 class Header extends Component {
      render() {
+          const name = this.props.name || "Anonymous"
+          const gravatar = this.props.email ?
+               <Gravatar options={{ email: this.props.email, secure: true }} style={styles.avatar} />
+               : null
           return (
                /* O Header ficou um pouco estrano pois não pegou a fonte desejada, lembrar de estudar a aplicação de fontes */
                <View style={styles.container}>
                     <View style={styles.rowContainer}>
                          <Image source={icon} style={styles.image} />
                          <Text style={styles.title}>Lambe Lambe</Text>
+                    </View>
+                    <View style={styles.userContainer}>
+                         <Text style={styles.user}>{name}</Text>
+                         {gravatar}
                     </View>
                </View>
           )
@@ -29,6 +39,8 @@ const styles = StyleSheet.create({
           borderBottomWidth: 1,
           borderColor: '#BBB',
           width: '100%',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
      },
      rowContainer: {
           flexDirection: 'row',
@@ -43,7 +55,29 @@ const styles = StyleSheet.create({
           color: '#000',
           height: 30,
           fontSize: 28
+     },
+     userContainer: {
+          flexDirection: 'row',
+          alignItems: 'center',
+     },
+     user: {
+          fontSize: 10,
+          color: "#888"
+     },
+     avatar: {
+          width: 30,
+          height: 30,
+          marginLeft: 10
      }
 })
 
-export default Header
+const mapStateToProps = ({ user }) => {
+     return{
+          email: user.email,
+          name: user.name,
+     }
+}
+
+// export default Header
+
+export default connect(mapStateToProps)(Header)
